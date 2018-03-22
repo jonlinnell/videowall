3b8d7e91c90226d2d579d2103f4fe3cb3557f82c
Jon Linnell <linnell.jon@gmail.com>
2018-03-22 13:50:41 +0000

    Outline screen layout

---
+++ src/components/ui/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div id='scr3x3'>3x3</div>
+        <div id='scr4_1'>4_1</div>
+        <div id='scr4_2'>4_2</div>
+        <div id='scr4_3'>4_3</div>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import './main.css'
+
+import App from './components/containers/App'
+
+render(<App />, document.getElementById('root'))
