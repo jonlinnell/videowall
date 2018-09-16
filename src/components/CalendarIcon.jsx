273392197ce7e86ac0c631a99e885da7278029f4
Jon Linnell <linnell.jon@gmail.com>
2018-09-16 21:14:34 +0100

    Add basic calendar icon

---
+++ src/components/CalendarIcon.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const CalendarIcon = props => (
+  <svg
+    viewBox="0 0 243 224.37"
+    {...props}
+  >
+    <defs>
+      <clipPath id="clip-path" transform="translate(0)">
+        <rect width="243" height="224.37" fill="none" />
+      </clipPath>
+    </defs>
+    <title>Calendar</title>
+    <g id="Layer_2" data-name="Layer 2">
+      <g id="Layer_1-2" data-name="Layer 1">
+        <g clipPath="url(#clip-path)">
+          <path fill="#FFF" d="M223,224.37H20a20,20,0,0,1-20-20V95.31a5.13,5.13,0,0,1,5.12-5.12H35.59a5.12,5.12,0,0,1,0,10.24H10.24v104A9.74,9.74,0,0,0,20,214.15H223a9.74,9.74,0,0,0,9.73-9.74l-.28-123.19H5.12A5.12,5.12,0,0,1,0,76.11V20A20,20,0,0,1,20,0H223a20,20,0,0,1,20,20V54.29a5.12,5.12,0,0,1-10.23,0V20A9.74,9.74,0,0,0,223,10.23H20A9.74,9.74,0,0,0,10.24,20V71H237.59a5.12,5.12,0,0,1,5.12,5.11L243,204.4a20,20,0,0,1-20,20" transform="translate(0)" />
+          <path fill="#FFF" d="M47.6,27.11a8,8,0,1,0,8,8,8,8,0,0,0-8-8m0,26.18A18.21,18.21,0,1,1,65.81,35.08,18.22,18.22,0,0,1,47.6,53.29" transform="translate(0)" />
+          <path fill="#FFF" d="M191.62,27.11a8,8,0,1,0,8,8,8,8,0,0,0-8-8m0,26.18a18.21,18.21,0,1,1,18.21-18.21,18.22,18.22,0,0,1-18.21,18.21" transform="translate(0)" />
+        </g>
+      </g>
+    </g>
+  </svg>
+)
+
+export default CalendarIcon
--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,9 +1,72 @@
 import React from 'react'
+import styled from 'styled-components'
+import moment from 'moment'
 
-const Event = ({ event: { title } }) => (
-  <div>
-    <p>{title}</p>
-  </div>
+import CalendarIcon from './CalendarIcon'
+
+const CalendarWrapper = styled.div`
+  position: relative;
+`
+
+const Date = styled.div`
+  position: absolute;
+  width: 200px;
+
+  top: 60px;
+
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+
+  text-align: center;
+
+  color: white;
+  font-family: "DIN Light";
+`
+
+const DayOfMonth = styled.p`
+  font-family: "DIN Light";
+  font-size: 4rem;
+  margin: 0;
+`
+
+const Month = styled.p`
+  font-family: "DIN Regular";
+  position: relative;
+  top: -12px;
+  font-size: 1.5rem;
+  text-transform: uppercase;
+  margin: 0;
+`
+
+const EventWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const EventTitle = styled.p`
+  margin: 0;
+  font-size: 2rem;
+  color: white;
+  font-family: "DIN Light";
+  padding-left: 2rem;
+`
+
+const Calendar = ({ date }) => (
+  <CalendarWrapper>
+    <CalendarIcon width={200} />
+    <Date>
+      <DayOfMonth>{moment(date).format('Do')}</DayOfMonth>
+      <Month>{moment(date).format('MMM')}</Month>
+    </Date>
+  </CalendarWrapper>
+)
+
+const Event = ({ event: { title, date } }) => (
+  <EventWrapper>
+    <Calendar date={date} />
+    <EventTitle>{title}</EventTitle>
+  </EventWrapper>
 )
 
 export default Event
