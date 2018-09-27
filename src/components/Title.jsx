b121825cfbaf039bd4dd41dcb97be5ecdada6f29
Jon Linnell <linnell.jon@gmail.com>
2018-09-27 07:29:01 +0100

    Make Title less huge

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 const Title = styled.h1`
   font-family: "DIN Light";
-  font-size: 8rem;
+  font-size: 6rem;
   color: rgb(255, 255, 255);
   margin: 0;
   padding: 0;
