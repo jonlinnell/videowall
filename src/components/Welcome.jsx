9c24654d85c1f9a27d07de1662ab6684b89d250f
Jon Linnell <linnell.jon@gmail.com>
2018-10-16 12:19:41 +0100

    Simplify Welcome component

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,76 +1,19 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import { Parallax } from 'react-spring'
-
-const Accolades = styled.div`
-  text-align: center;
-`
 
 const WelcomeText = styled.p`
-  font-size: ${({ large }) => (large ? '17rem' : '11rem')};
+  font-size: ${({ large }) => (large ? '256px' : '192px')};
   margin: 0;
   font-family: 'DIN Extlight';
   text-align: center;
   color: rgb(255, 255, 255);
 `
 
-class Welcome extends PureComponent {
-  constructor(props) {
-    super(props)
-
-    this.parallaxRef = React.createRef()
-
-    this.state = {
-      page: 0,
-    }
-  }
-
-  componentDidMount() {
-    this.scrollInterval = setInterval(() => this.scroll(), 4000)
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.scrollInterval)
-  }
-
-  scrollInterval = null
-
-  scroll() {
-    this.setState({ page: this.state.page === 1 ? 0 : 1 }, () => {
-      this.parallaxRef.current.scrollTo(this.state.page)
-    })
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <WelcomeText>Welcome to</WelcomeText>
-        <WelcomeText large>Loughborough University London</WelcomeText>
-        <Accolades>
-          <Parallax ref={this.parallaxRef} pages={1}>
-            <Parallax.Layer offset={0}>
-              <p>Test 1</p>
-            </Parallax.Layer>
-            <Parallax.Layer offset={0}>
-              <p>Test 2</p>
-            </Parallax.Layer>
-            <Parallax.Layer offset={0}>
-              <p>Test 3</p>
-            </Parallax.Layer>
-            <Parallax.Layer offset={1}>
-              <p>Test 4</p>
-            </Parallax.Layer>
-            <Parallax.Layer offset={1}>
-              <p>Test 5</p>
-            </Parallax.Layer>
-            <Parallax.Layer offset={1}>
-              <p>Test 6</p>
-            </Parallax.Layer>
-          </Parallax>
-        </Accolades>
-      </React.Fragment>
-    )
-  }
-}
+const Welcome = () => (
+  <div>
+    <WelcomeText>Welcome to</WelcomeText>
+    <WelcomeText large>Loughborough University London</WelcomeText>
+  </div>
+)
 
 export default Welcome
