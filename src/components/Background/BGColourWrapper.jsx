20ffdb44d8b8ec979e1917e4d57ab7571e1aca54
Jon Linnell <linnell.jon@gmail.com>
2019-02-28 14:57:51 +0000

    Refactor BGColourWrapper to use hooks

--- src/components/Background/BGColourWrapper.jsx
+++ src/components/Background/BGColourWrapper.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import moment from 'moment-timezone'
 import styled from 'styled-components'
@@ -7,71 +7,13 @@ import { coords } from '../../../config.json'
 
 const { latitude, longitude, timezone } = coords
 
-class BGColourWrapper extends PureComponent {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      night: false,
-      sunset: null,
-      sunrise: null,
-    }
-  }
-
-  componentDidMount() {
-    this.fetchSunsetTimes()
-    this.fetchSunsetTimesInterval = setInterval(() => {
-      this.fetchSunsetTimes()
-    }, 6 * 1000 * 60 * 60) // 6 hours
-
-    this.nightCheckInterval = setInterval(() => {
-      this.isItNight()
-    }, 5 * 1000 * 60) // 5 minutes
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.nightCheckInterval)
-  }
-
-  nightCheckInterval = null
-  fetchSunsetTimesInterval = null
-
-  fetchSunsetTimes = () => {
-    axios
-      .get(`https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&formatted=0`)
-      .then(({ data: { results } }) => {
-        const { sunset, sunrise } = results
-
-        this.setState({
-          sunset: moment(sunset, timezone),
-          sunrise: moment(sunrise, timezone),
-        }, () => this.isItNight())
-      })
-  }
-
-  isItNight = () => {
-    const { sunset, sunrise } = this.state
-    const now = moment().tz(timezone)
-
-    this.setState({ night: ((now.isAfter(sunset)) || (now.isBefore(sunrise))) })
-  }
-
-  render() {
-    return (
-      <BGColour nightMode={this.state.night}>
-        { this.props.children }
-      </BGColour>
-    )
-  }
-}
-
 const BGColour = styled.div`
   height: 3240px;
   width: 7680px;
 
   position: relative;
 
-  &::before{
+  &::before {
     content: '';
     width: 100%;
     height: 100%;
@@ -81,11 +23,71 @@ const BGColour = styled.div`
     top: 0;
 
     z-index: 1;
-    background-color: ${({ theme: { colours }, nightMode }) => (nightMode ? colours.sunset : colours.mulberry)};
+    background-color: ${({ theme: { colours }, nightMode }) =>
+    (nightMode ? colours.sunset : colours.mulberry)};
     mix-blend-mode: screen;
 
-    transition: background-color ease-in 30s;
+    transition: background-color ease-in 3s;
   }
 `
+
+const BGColourWrapper = ({ children }) => {
+  const [cycleData, setCycleData] = useState()
+  const [now, setNow] = useState(moment().tz(timezone))
+
+  const fetchCycleData = async () => {
+    const response = await axios.get(`https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&formatted=0`)
+
+    // // Leaving this in for innevitable future debugging...
+    // const response = {
+    //   data: {
+    //     results: {
+    //       sunrise: '2019-02-28T06:46:34+00:00',
+    //       sunset: '2019-02-28T13:28:33+00:00',
+    //       solar_noon: '2019-02-28T12:12:33+00:00',
+    //       day_length: 39119,
+    //       civil_twilight_begin: '2019-02-28T06:13:06+00:00',
+    //       civil_twilight_end: '2019-02-28T18:12:01+00:00',
+    //       nautical_twilight_begin: '2019-02-28T05:34:28+00:00',
+    //       nautical_twilight_end: '2019-02-28T18:50:39+00:00',
+    //       astronomical_twilight_begin: '2019-02-28T04:55:35+00:00',
+    //       astronomical_twilight_end: '2019-02-28T19:29:31+00:00',
+    //     },
+    //     status: 'OK',
+    //   },
+    // }
+
+    if (!response || response.data.status !== 'OK') {
+      console.error('There was an error loading solar cycle data.')
+    } else {
+      setCycleData(response.data.results)
+    }
+  }
+
+  useEffect(() => {
+    fetchCycleData()
+    const sunrisePollInterval = setInterval(() => fetchCycleData(), 6 * 1000 * 60 * 60)
+
+    return () => clearInterval(sunrisePollInterval)
+  }, [])
+
+  useEffect(() => {
+    const updateNowInterval = setInterval(() => setNow(moment().tz(timezone)), 5 * 1000 * 60)
+
+    return () => clearInterval(updateNowInterval)
+  }, [])
+
+  return (
+    <BGColour
+      nightMode={
+        cycleData
+          ? now.isBefore(moment(cycleData.sunrise)) || now.isAfter(moment(cycleData.sunset))
+          : false
+      }
+    >
+      {children}
+    </BGColour>
+  )
+}
 
 export default BGColourWrapper
